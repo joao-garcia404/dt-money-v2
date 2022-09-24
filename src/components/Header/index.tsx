5e4682916176999af99b5b8dc741160baceaaeef
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-09-24 20:04:14 -0300

    Feat: Summary calc

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
 import * as Dialog from "@radix-ui/react-dialog";
 
-import logoImg from "../../assets/logo.svg";
 import { NewTransactionModal } from "../NewTransactionModal";
+
+import logoImg from "../../assets/logo.svg";
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,37 +1,66 @@
+import { useTheme } from "styled-components";
+import { useTransactions } from "../../hooks/useTransactions";
+
+import { priceFormatter } from "../../utils/formatter";
+
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
+  const theme = useTheme();
+
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.incomes += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcomes += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      incomes: 0,
+      outcomes: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
           <span>Entradas</span>
 
-          <ArrowCircleUp size={32} color="#00b37e" />
+          <ArrowCircleUp size={32} color={theme["green-300"]} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.incomes)}</strong>
       </SummaryCard>
 
       <SummaryCard>
         <header>
           <span>Saídas</span>
 
-          <ArrowCircleDown size={32} color="#F75A68" />
+          <ArrowCircleDown size={32} color={theme["red-300"]} />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>{priceFormatter.format(summary.outcomes)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
 
-          <CurrencyDollar size={32} color="#fff" />
+          <CurrencyDollar size={32} color={theme.white} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
